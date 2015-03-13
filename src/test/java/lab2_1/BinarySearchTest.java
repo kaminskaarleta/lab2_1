11c1a37e67b7d4fb1dc9d61f0a3e8d0e2c2ed807
kaminskaarleta <180195@edu.p.lodz.pl>
2015-03-13 11:19:41 +0100

    Added new tests to BinarySearchTest

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -16,35 +16,46 @@ public class BinarySearchTest {
 	@Test
 	public void seqLength1_shouldFind() {
 		int seq[] = {1};
-		SearchResult result = BinarySearch.search(1, seq);
+		int key = 1;
+		SearchResult result = BinarySearch.search(key, seq);
 		assertThat(result.isFound(), is(true));		
+		assertThat(result.getPosition(), is(key));
+
 	}
 	
 	@Test
 	public void seqLength1_shouldNotFind() {
 		int seq[] = {1};
-		SearchResult result = BinarySearch.search(2, seq);
-		assertThat(result.isFound(), is(false));		
+		int key = 2;
+		SearchResult result = BinarySearch.search(key, seq);
+		assertThat(result.isFound(), is(false));	
+		assertThat(result.getPosition(), is(-1));
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtFirstIndex() {
 		int seq[] = {1,2,3};
-		SearchResult result = BinarySearch.search(1, seq);
-		assertThat(result.getPosition(), is(1));		
+		int key = 1;
+		SearchResult result = BinarySearch.search(key, seq);
+		assertThat(result.isFound(), is(true));
+		assertThat(result.getPosition(), is(key));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtLastIndex() {
 		int seq[] = {1,2,3};
-		SearchResult result = BinarySearch.search(3, seq);
+		int key = 3;
+		SearchResult result = BinarySearch.search(key, seq);
+		assertThat(result.isFound(), is(true));
 		assertThat(result.getPosition(), is(seq.length));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtCentreIndex() {
 		int seq[] = {1,2,3};
-		SearchResult result = BinarySearch.search(2, seq);
+		int key = 2;
+		SearchResult result = BinarySearch.search(key, seq);
+		assertThat(result.isFound(), is(true));
 		assertThat(result.getPosition(), is((seq.length+1)/2));		
 	}
 	
@@ -52,7 +63,14 @@ public class BinarySearchTest {
 	public void seqLengthGreater1_shouldNotFind() {
 		int seq[] = {1,2,3};
 		SearchResult result = BinarySearch.search(5, seq);
-		assertThat(result.isFound(), is(false));		
+		assertThat(result.isFound(), is(false));
+		assertThat(result.getPosition(), is(-1));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void seqLength0_shouldGetException() {
+		int seq[] = {};
+		SearchResult result = BinarySearch.search(5, seq);
 	}
 	
 }
