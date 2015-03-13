3fc32082daad0ed2c6248012b3c6d98c2bec2bc1
kaminskaarleta <180195@edu.p.lodz.pl>
2015-03-13 11:08:57 +0100

    refactored BinarySearchTest

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -1,7 +1,9 @@
 package lab2_1;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
@@ -9,45 +11,48 @@ import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest {	
 	
+	
+	
 	@Test
 	public void seqLength1_shouldFind() {
 		int seq[] = {1};
 		SearchResult result = BinarySearch.search(1, seq);
-		assertTrue(result.isFound());		
+		assertThat(result.isFound(), is(true));		
 	}
 	
 	@Test
 	public void seqLength1_shouldNotFind() {
 		int seq[] = {1};
 		SearchResult result = BinarySearch.search(2, seq);
-		assertFalse(result.isFound());		
+		assertThat(result.isFound(), is(false));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtFirstIndex() {
 		int seq[] = {1,2,3};
 		SearchResult result = BinarySearch.search(1, seq);
-		assertTrue(result.getPosition()==1);		
+		assertThat(result.getPosition(), is(1));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtLastIndex() {
 		int seq[] = {1,2,3};
 		SearchResult result = BinarySearch.search(3, seq);
-		assertTrue(result.getPosition()==seq.length);		
+		assertThat(result.getPosition(), is(seq.length));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldFindAtCentreIndex() {
 		int seq[] = {1,2,3};
 		SearchResult result = BinarySearch.search(2, seq);
-		assertTrue(result.getPosition()==(seq.length+1)/2);		
+		assertThat(result.getPosition(), is((seq.length+1)/2));		
 	}
 	
 	@Test
 	public void seqLengthGreater1_shouldNotFind() {
 		int seq[] = {1,2,3};
 		SearchResult result = BinarySearch.search(5, seq);
-		assertFalse(result.isFound());		
+		assertThat(result.isFound(), is(false));		
 	}
+	
 }
